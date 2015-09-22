932896e0131d9c524c7b020c39aee04c1cc3acc9
irina <irina@me.com>
2015-09-22 16:31:32 +0300

    accessors

--- src/a/b/Clazz1.java
+++ src/a/b/Clazz1.java
@@ -6,4 +6,12 @@ package a.b;
 public class Clazz1 {
 
     String str1;
+
+    public String getStr1() {
+        return str1;
+    }
+
+    public void setStr1(String str1) {
+        this.str1 = str1;
+    }
 }
