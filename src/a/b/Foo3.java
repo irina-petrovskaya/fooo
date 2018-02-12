48543d55247b04232dfa6435c24bd82824033b6c
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-02-12 15:27:58 +0300

    Foo3.java

---
+++ src/a/b/Foo3.java
@@ -0,0 +1,10 @@
+package a.b;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 2/12/2018.
+ * Project: fooo
+ * *******************************
+ */
+public class Foo3 {
+}
